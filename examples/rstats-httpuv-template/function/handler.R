df176cfafb8b587190b1b1210126d8e5f4e1a1ad
Peter Solymos <psolymos@gmail.com>
2020-01-16 20:41:37 -0700

    Removing examples from template repo

--- examples/rstats-httpuv-template/function/handler.R
+++
@@ -1,7 +0,0 @@
-handle <- function(req) {
-  input <- req[["rook.input"]]
-  postdata <- input$read_lines()
-  jsonlite::toJSON(
-    paste0("Hello ", jsonlite::fromJSON(
-      paste(postdata)), "!"))
-}
