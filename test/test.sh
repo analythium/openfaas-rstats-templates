5df7655d7002c534061d8a6bb3b721eb9e03831b
Peter Solymos <psolymos@gmail.com>
2021-09-02 01:02:37 -0600

    Testing updated
    Signed-off-by: Peter Solymos <psolymos@gmail.com>

--- test/test.sh
+++ test/test.sh
@@ -9,15 +9,39 @@ echo -n $PASSWORD | faas-cli login --username admin --password-stdin
 #faas-cli template pull https://github.com/analythium/openfaas-rstats-templates
 
 # Which template to use
-export TEMPLATE_NAME="rstats-base-plumber"
+#export TEMPLATE_NAME="rstats-ubuntu-plumber"
+#export TEMPLATE_NAME="rstats-ubuntu-httpuv"
+#export TEMPLATE_NAME="rstats-ubuntu-fiery"
+#export TEMPLATE_NAME="rstats-ubuntu-beakr"
+#export TEMPLATE_NAME="rstats-ubuntu-ambiorix"
+#export TEMPLATE_NAME="rstats-ubuntu"
+
+#export TEMPLATE_NAME="rstats-base-plumber"
+#export TEMPLATE_NAME="rstats-base-httpuv"
+#export TEMPLATE_NAME="rstats-base-fiery"
+#export TEMPLATE_NAME="rstats-base-beakr"
+#export TEMPLATE_NAME="rstats-base-ambiorix"
+#export TEMPLATE_NAME="rstats-base"
+
+#export TEMPLATE_NAME="rstats-minimal-plumber"
+#export TEMPLATE_NAME="rstats-minimal-httpuv"
+#export TEMPLATE_NAME="rstats-minimal-fiery"
+#export TEMPLATE_NAME="rstats-minimal-beakr"
+#export TEMPLATE_NAME="rstats-minimal-ambiorix"
+#export TEMPLATE_NAME="rstats-minimal"
 
 # Create Hello World example
 export FUNCTION_NAME=r-$TEMPLATE_NAME
 faas-cli new --lang $TEMPLATE_NAME $FUNCTION_NAME --prefix=$OPENFAAS_PREFIX
 faas-cli up -f $FUNCTION_NAME.yml
 
+# Inspect logs
+faas-cli logs $FUNCTION_NAME
+
 # Test the function
-curl $OPENFAAS_URL/function/$FUNCTION_NAME -d '"World"'
+curl $OPENFAAS_URL/function/$FUNCTION_NAME \
+  -d '"World"' \
+  -H 'Content-Type: application/json'
 
 # Remove the function and files
 faas-cli remove -f $FUNCTION_NAME.yml
